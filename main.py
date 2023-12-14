fc5ef2735e73af5d481b42a00000f2d77da13240
student <pavlokiht2005@gmail.com>
2023-12-14 14:01:46 +0100

    initial commit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class MyForm(QDialog):
             self.ui = Ui_Dialog()
             self.ui.setupUi(self)
             self.ui.save.clicked.connect(self.wyswietl)
+            self.ui.pushButton_savetofile.clicked.connect(self.wyswietl)
             self.show()
 
         def wyswietl(self):
@@ -20,19 +21,28 @@ class MyForm(QDialog):
             pesel = self.ui.pesel.text().strip()
             numbertl = self.ui.numberofphone.text().strip()
 
+            lista = ["imie ",name, "nazwisko ",secondname, "pesel " ,pesel, "numertelefonu ",numbertl]
+
             dane = name + " " + secondname
+
+            waga = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
+            suma_kontrolna = sum(int(pesel[i]) * waga[i] for i in range(10))
+            numer_kontrolny = (10 - (suma_kontrolna % 10)) % 10
+
             if len(pesel) != 11 or not pesel.isnumeric():
                 blad = QMessageBox()
                 blad.setText("Pesel nie zawiera numeru lub nie ma 11 znaków")
                 blad.exec()
                 return
 
-            waga = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
-            suma_kontrolna = sum(int(pesel[i]) * waga[i] for i in range(10))
-            numer_kontrolny = (10 - (suma_kontrolna % 10)) % 10
-
             if numer_kontrolny == int(pesel[10]):
                 self.ui.listofuser.addItem(dane)
+                plik = open(f'{name}.txt', 'w')
+                lista_nowa = []
+                for i in lista: lista_nowa.append(i + '\n')
+                plik.writelines(lista_nowa)
+                plik.close()
+
             else:
                 blad = QMessageBox()
                 blad.setInformativeText("Nieprawidłowy numer PESEL")
